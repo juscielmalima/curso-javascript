0806a46544d94053449caab8af3e739e402d6771
Juscielma <juscielma.med@gmail.com>
2019-06-03 00:25:28 -0300

    aprendendo IF Else e For

---
+++ aula09/ex005.js
@@ -0,0 +1,48 @@
+// function verificaQuemEMaisVelho(idade1, idade2) {
+//     if (idade1 > idade2) {
+//         return ("Idade 1 é o mais velho")
+//     }
+//     else if (idade2 > idade1) {
+//         return ("Idade 2 é o mais velho")
+//     }
+//     else {
+//         return ("Mesma idade")
+//     }
+// }
+
+// function verificaQuemEMaisNovo(idade1, idade2) {
+//     if (idade1 < idade2) {
+//         alert("Idade 1 é o mais novo")
+//     }
+//     else if (idade2 < idade1) {
+//         alert("Idade 2 é o mais novo")
+//     }
+//     else {
+//         alert("Mesma idade")
+//     }
+// }
+
+// verificaQuemEMaisVelho(30, 18)
+// verificaQuemEMaisNovo(30, 18)
+
+
+// for (var loop = 0; loop <= 5; loop++) {
+//     alert('ESTOU NA VOLTA: ' + loop)
+// }
+
+var array = [1, 4, 12, 'gato', 'arvore']
+
+// alert(array);
+
+// console.log(array[4])
+
+for (var i = 0; i < array.length; i++) {
+    alert(array[i])
+}
+
+
+
+
+for (let index = 0; index < array.length; index++) {
+    
+}
